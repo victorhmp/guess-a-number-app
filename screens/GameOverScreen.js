bb18951bb680720648f6912f09c73e888ffb0ad4
victorhmp <victor2142@gmail.com>
2020-10-17 11:53:54 -0300

    Add a local image to the game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, Image } from "react-native";
 
 import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <BodyText>The Game is Over!</BodyText>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageWrapper}>
+        <Image
+          style={styles.image}
+          resizeMode="cover"
+          source={require("../assets/success.png")}
+        />
+      </View>
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
@@ -19,6 +27,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageWrapper: {
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
